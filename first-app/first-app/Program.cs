b5fd10df9d003567ad2f698392d27bf78c34638f
M Suleman <163521322+MSulemankashif@users.noreply.github.com>
2024-12-22 16:02:03 +0500

    calculator

---
+++ first-app/first-app/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace first_app
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----Building Calculator----");
+            Console.Write("Enter Your first number ");
+            int number1 = Convert.ToInt16(Console.ReadLine());
+
+
+            Console.Write("Enter Your second number ");
+            int number2 = Convert.ToInt16(Console.ReadLine());
+
+            Console.Write("Your Sum is : ");
+            Console.Write(number1 + number2);
+            Console.ReadKey();
+        }
+    }
+}
